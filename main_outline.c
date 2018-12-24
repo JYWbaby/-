90b8c8584366fea2115c95c4e982abad532439fb
Ppop88 <46025635+Ppop88@users.noreply.github.com>
2018-12-25 00:22:47 +0800

    Update main_outline.c

--- main_outline.c
+++ main_outline.c
@@ -1,3 +1,73 @@
+#include <headdfile.h>
+//int value1; 
+//uint8_t dir; /* 记录编码器旋转方向1 */
+   
+
+
+
+void mac(void)           // 通用io口输入输出函数
+{
+
+	GPIO_QuickInit(HW_GPIOD, 11, kGPIO_Mode_OPP);             //io口简单初始化
+	GPIO_WriteBit(HW_GPIOD, 11, 1);                           //制定引脚配置高电平
+                  
+	GPIO_QuickInit(HW_GPIOD, 9, kGPIO_Mode_OPP);              //io口简单初始化
+	GPIO_WriteBit(HW_GPIOD, 9, 0);                            //制定引脚配置高电平
+	
+}
+
+
+
+
+int main(void)
+{
+    initx();  //初始化函数
+	  mac();  // 通用io口输入输出函数 调pid给的是驱动的一个/这里可以不用
+	 
+   
+
+
+	
+	while(1)
+    {
+    printf("HelloWorld!!!\r\n"); 
+        GPIO_ToggleBit(HW_GPIOE, 6);
+        DelayMs(500);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "pid.h"
 #include "ftm.h"
 #include "uart.h"
