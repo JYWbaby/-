b5683805d734ecf0a3563209738dec805832ed3f
JYWbaby <35770621+JYWbaby@users.noreply.github.com>
2018-12-25 11:08:33 +0800

    Update main.c

--- main.c
+++ main.c
@@ -28,12 +28,10 @@ int main(void)
 
 
 	
-	while(1)
-    {
-    printf("HelloWorld!!!\r\n"); 
-        GPIO_ToggleBit(HW_GPIOE, 6);
-        DelayMs(500);
-    }
+	while(1){
+    scan_reed();//干簧管检测停车标志
+		 //如果检测到终点，则stop（）
+	}
 }
 
 
@@ -68,39 +66,7 @@ int main(void)
 
 
 
-#include "pid.h"
-#include "ftm.h"
-#include "uart.h"
-#include "shiboqi.h"
-#include "pit.h"
-#include "gpio.h"
 
-#define OFF 0
-#define SET 1
-
-
-
-
-float speed;
-float sensor_1,sensor_2,sensor_3,sensor_4;//4路传感器反馈值，分别为左2，左1，右1，右2
-
-/*
-//中断读取编码器脉冲
-//反馈给控制程序
-static void PIT_ISR(void)
-{
-	int value;
-	uint8_t  direction;
-	
-	FTM_QD_GetData(HW_FTM1, &value, &direction);
-	FTM_QD_ClearCount(HW_FTM1);
-	speed=value;//(25*0.001);
-	
-  control(sensor_1,sensor_2,sensor_3,sensor_4，speed);
-	
-	//oled显示
-}
-*/
 
 
 
@@ -127,8 +93,6 @@ int main(){
 		 //如果检测到终点，则stop（）
 			
 			
-		scan_sensor();	//adc_1、adc_2、adc_3读取、adc_4读取
-			
-			//根据读取到的值来确定速度和方向
+		
 		}      
 	}
