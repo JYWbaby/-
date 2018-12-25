ea3e3bddfcb5dc933e3ec47389a6007be55f2b17
JYWbaby <35770621+JYWbaby@users.noreply.github.com>
2018-12-25 11:02:57 +0800

    Update Adc_user.c

--- Adc_user.c
+++ Adc_user.c
@@ -3,7 +3,7 @@
 void ADC_user_init()
 {
 	
-	/* 初始化ADC模块 ADC0_SE19_BM0 */
+	/* 初始化ADC模块 ADC0*/
     ADC_InitTypeDef ADC_InitStruct1;
     ADC_InitStruct1.instance = HW_ADC0;
     ADC_InitStruct1.clockDiv = kADC_ClockDiv2; /* ADC采样时钟2分频 */
@@ -15,7 +15,7 @@ void ADC_user_init()
     ADC_InitStruct1.vref = kADC_VoltageVREF;                       /* 使用外部VERFH VREFL 作为模拟电压参考 */
     ADC_Init(&ADC_InitStruct1);
 	
-	/* 初始化ADC模块 ADC0_SE19_BM0 */
+	/* 初始化ADC模块 ADC1 */
     ADC_InitStruct1.instance = HW_ADC1;
     ADC_InitStruct1.clockDiv = kADC_ClockDiv2; /* ADC采样时钟2分频 */
     ADC_InitStruct1.resolutionMode = kADC_SingleDiff12or13;
@@ -27,11 +27,9 @@ void ADC_user_init()
     ADC_Init(&ADC_InitStruct1);
     
     /* 初始化对应引脚 */
-    /* DM0引脚为专门的模拟引脚 ADC时 无需设置复用  DM0也无法当做普通的数字引脚 */
+    /* 引脚为专门的模拟引脚*/
     
-    /* 启动一次ADC转换 */
-    ADC_StartConversion(HW_ADC0, 12, kADC_MuxA);
+    
 }
-//ADC快速读值：int32_t ADC_QuickReadValue(); 
 
 
